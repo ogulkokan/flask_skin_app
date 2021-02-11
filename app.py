9b78c2764cd41e145414a511109fe699f561ffac
onurgulkokan <onurgulkokan@gmail.com>
2021-02-11 20:09:32 +0100

    heroku fail test

--- app.py
+++ app.py
@@ -231,5 +231,8 @@ def results():
 def login():
     return render_template('login.html')
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
