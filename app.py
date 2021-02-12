d870f59cd1ffbe0f106fc0e4358250f25ed61eb2
onurgulkokan <onurgulkokan@gmail.com>
2021-02-12 03:18:28 +0100

    procfile test

--- app.py
+++ app.py
@@ -238,5 +238,5 @@ def login():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
